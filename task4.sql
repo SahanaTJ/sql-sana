a7d8eaa65571e7b48f3ae9b537c8ac124c03b744
SahanaTJ <113516386+SahanaTJ@users.noreply.github.com>
2022-11-16 23:40:03 -0800

    Delete task4.sql

--- task4.sql
+++
@@ -1,4 +0,0 @@
-
-create database home_detatils;
-use home_detatils;
-create table homeloan(id int auto_increment primary key,home_name varchar(30),home_number int not null,no_of_intrest int not null
